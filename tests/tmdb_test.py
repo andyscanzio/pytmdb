cf9e0ec834cb4494b229d79842191d30b0f9b3e3
Andrea Scanzio <andyscanzio@gmail.com>
2023-08-12 16:38:40 +0200

    Better test coverage

--- tests/tmdb_test.py
+++ tests/tmdb_test.py
@@ -3,8 +3,18 @@ import pytest
 from pytmdb.tmdb import TMDB
 
 
-def test_tmdb():
+def test_tmdb_version():
     with pytest.raises(ValueError) as excinfo:
         _ = TMDB(version=4)
 
     assert "Only version 3 supported" == str(excinfo.value)
+
+
+def test_tmdb_without_key():
+    tmdb = TMDB()
+    assert tmdb.key is not None
+
+
+def test_tmdb_with_key():
+    tmdb = TMDB(api_key="aa")
+    assert tmdb.key is not None
